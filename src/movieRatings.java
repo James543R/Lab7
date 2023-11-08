d60418bc80cde8999728dccddfaa9c9d88c1945d
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2023-11-08 14:51:46 -0500

    Update movieRatings.java

--- src/movieRatings.java
+++ src/movieRatings.java
@@ -1,27 +1,40 @@
 import java.util.*;
+
+/**
+ * movieRatings - A class that contains the rankMoveies() method.
+ * @author Nathaniel Serrano
+ * @version 8 November 2023
+ *
+ */
 public class movieRatings {
 	
+	/**
+	 * rankMovies() - A method that takes a TreeMap of Strings and TreeSets of movie titles and 
+	 * returns a list ranking the most most common to the least common movies in the list.
+	 * @param map - TreeMap<String, TreeSet<String>>
+	 * @return rankings - List<String>
+	 */
 	public static List<String> rankMovies(TreeMap<String, TreeSet<String>> map) {
 		List<String> rankings = new ArrayList<String>();
-		String[] values = (String[])map.values().toArray();
+		Object[] array = map.values().toArray();
+		String[] values = Arrays.copyOf(array, array.length, String[].class);
 		List<String> listValues = Arrays.asList(values);
-		
 	    while (listValues.size()>0) {
-			int freq = 0;
-		    String res = "";
+			int max = 0;
+		    String mostPopular = "";
 		    for (int i = 0; i < listValues.size(); i++) {
 		      int count = 0;
 		      for (int j = i + 1; j < listValues.size(); j++) {
 		        if (listValues.get(j).equals(listValues.get(i)))
 		          count++;
 		      }
-		      if (count >= freq) {
-		        res = listValues.get(i);
-		        freq = count;
+		      if (count >= max) {
+		        mostPopular = listValues.get(i);
+		        max = count;
 		      }
-		      rankings.add(res);
-		      listValues.remove(res);
 		    }
+		      rankings.add(mostPopular);
+		      listValues.remove(mostPopular);
 	    }
 		return rankings;
 	}
